10f8bdf33b98d5c55e3a4cead8863b33008dfc54
Uffe Petersen <uffe.petersen@ist.com>
2017-12-21 19:46:57 +0100

    Added account profile with update

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -36,6 +36,9 @@
 
                         <ul class="dropdown-menu">
                             <li>
+                                <a href="{{ route('account.index') }}">Account</a>
+                            </li>
+                            <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::group(['prefix' => 'account', 'middleware' => ['auth'], 'as' => 'account.'], function() {
+    Route::get('/', 'Account\AccountController@index')->name('index');
+
+    /** Profile */
+    Route::get('/profile', 'Account\ProfileController@index')->name('profile.index');
+    Route::post('/profile', 'Account\ProfileController@store')->name('profile.store');
+
+});

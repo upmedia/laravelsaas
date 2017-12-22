3fd3f9620761fe9b87c099967fea54ba412bf3af
Uffe Petersen <mail@upmedia.dk>
2017-12-22 09:22:39 +0100

    Added helpers and menu selection

--- resources/views/account/partials/_navigation.blade.php
+++ resources/views/account/partials/_navigation.blade.php
@@ -1,11 +1,11 @@
 <ul class="nav nav-pills nav-stacked">
-    <li>
+    <li class="{{ return_if(on_page('account'), 'active') }}">
         <a href="{{ route('account.index') }}">Account overview</a>
     </li>
-    <li>
+    <li class="{{ return_if(on_page('account/profile'), 'active') }}">
         <a href="{{ route('account.profile.index') }}">Profile</a>
     </li>
-    <li>
+    <li class="{{ return_if(on_page('account/password'), 'active') }}">
         <a href="{{ route('account.password.index') }}">Change password</a>
     </li>
 </ul>

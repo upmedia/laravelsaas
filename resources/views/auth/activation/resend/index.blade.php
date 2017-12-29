aee3518e397e7e39efdfccac74e310199a3cf302
Uffe Petersen <mail@upmedia.dk>
2017-12-29 11:09:23 +0100

    Added last authentication

---
+++ resources/views/auth/activation/resend/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Resend password confirmation</div>
+
+                    <div class="panel-body">
+                        <form action="{{ route('activation.resend.store') }}" class="form-horizontal" method="post">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail address</label>
+                                <div class="col-md-6">
+                                    <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}">
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Resend
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::group(['prefix' => 'account', 'middleware' => ['auth'], 'as' => 'account.
 
 });
 
-Route::group(['prefix' => 'activation', 'as' => 'activation.', 'middleware' => ['guest', 'confirmation_token.expired:/']], function() {
+Route::group(['prefix' => 'activation', 'as' => 'activation.', 'middleware' => ['guest']], function() {
+    Route::get('/resend', 'Auth\ActivationResendController@index')->name('resend');
+    Route::post('/resend', 'Auth\ActivationResendController@store')->name('resend.store');
     Route::get('/{confirmation_token}', 'Auth\ActivationController@activate')->name('activate');
 });
